09129944a357bad02821d08f18ceaffe75b74a41
MSturges <Sturges3232@gmail.com>
2017-01-17 08:36:22 -0700

    pre refactoring of projects

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './nav-footer/navigation';
+import ProfileHeader from './profile/header';
 import Footer from './nav-footer/footer';
-import ProfileIndex from './profile_index';
 
 export default class App extends Component {
   render() {
     return (
       <div>
       <NavBar />
-      <ProfileIndex/>
+      <ProfileHeader/>
       {this.props.children}
       <Footer/>
       </div>
