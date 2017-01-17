43569b45c43f12b8f655fb4914da25c909cdf200
MSturges <Sturges3232@gmail.com>
2017-01-17 16:03:01 -0700

    deploy with footer update

--- src/components/nav-footer/footer.js
+++ src/components/nav-footer/footer.js
@@ -6,12 +6,14 @@ const Footer = () => {
     <div>
     <div className="skills-break-two"></div>
       <div className="footer">
+      <a className="fa fa-github footer-icon" href="https://github.com/msturges"><span>Git Hub</span></a>
+      <a className="fa fa-linkedin footer-icon" href="https://www.linkedin.com/in/max-r-sturges"><span>Linkedin</span></a>
+      <a className="fa fa-envelope footer-icon" href="mailto:max.r.sturges@gmail.com"><span>max.r.sturges@gmail.com</span></a>
+      <a className="fa fa-phone footer-icon" href="#"><span>(914) 582-0029</span></a>
     </div>
     </div>
 
   );
 }
-
-
 
 export default Footer;
